f51eab97bcb04a5b9fdee650adaab5966a9b66ca
heyosseus <ratiruxadzee@gmail.com>
2023-09-20 14:27:20 +0400

    feat: create header component

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="py-6 px-10 w-full bg-blue-950 flex justify-between items-center">
+    <img src="@/assets/autotechno.png" alt="" />
+    <div class="flex items-center space-x-4">
+      <router-link to="#" class="text-white text-2xl">Next page</router-link>
+      <img src="@/assets/next.png" alt="" class="bg-white mt-1 w-5 h-5 rounded-full p-1"/>
+    </div>
+  </div>
+</template>
+<script setup></script>
