f034f4dbe4863e1805d389ae23c3dda0a1514486
heyosseus <ratiruxadzee@gmail.com>
2023-09-20 16:03:30 +0400

    feat: create form components and extract them into seperate files

---
+++ src/components/BaseForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex items-center space-x-10 mt-4">
+    <label for="დასახელება" class="text-lg text-center w-44">დასახელება:</label>
+    <input type="text" class="w-full ml-auto bg-gray-200 p-4 rounded-full mt-2 outline-0" />
+  </div>
+  <div class="flex items-center space-x-10 mt-10">
+    <label for="გამზომილება" class="text-lg text-center w-44">გამზომილება:</label>
+    <input type="text" class="w-full ml-auto bg-gray-200 p-4 rounded-full mt-2 outline-0" />
+  </div>
+  <div class="flex items-center space-x-10 mt-10">
+    <label for="რაოდენობა" class="text-lg text-center w-44">გასული კვირის რაოდენობა:</label>
+    <input type="text" class="w-full ml-auto bg-gray-200 p-4 rounded-full mt-2 outline-0" />
+  </div>
+  <div class="flex items-center space-x-10 mt-10">
+    <label for="კვოტა" class="text-lg text-center w-44">მიმდინარე კვირის კვოტა:</label>
+    <input type="text" class="w-full ml-auto bg-gray-200 p-4 rounded-full mt-2 outline-0" />
+  </div>
+  <div class="flex items-center space-x-10 mt-10">
+    <label for="შენიშვნა" class="text-lg text-center w-44">შენიშვნა:</label>
+    <input type="text" class="w-full ml-auto bg-gray-200 p-4 rounded-full mt-2 outline-0" />
+  </div>
+</template>
