ab66d81db92d9873f38b99fb00763af13bdb257c
heyosseus <ratiruxadzee@gmail.com>
2023-09-20 14:27:35 +0400

    chore: minor updates

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="flex flex-col ">
+    <base-header />
+    <div class="p-6">
+      <h1 class="text-2xl">Weekly plan:</h1>
+    </div>
+  </div>
 </template>
+<script setup>
+import BaseHeader from '../components/BaseHeader.vue'
+</script>
